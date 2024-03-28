881ca761779128b262978d7d9b367d90a6d22d94
Khalidabdi1 <127331761+Khalidabdi1@users.noreply.github.com>
2024-03-28 20:27:05 +0300

    odd or even
    A Java program that tells someone whether the number they entered is odd or even

---
+++ even.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class even{
+
+
+public static void main(String[]args){
+Scanner input =new Scanner(System.in);
+int x =input.nextInt();
+
+if(x % 2 ==0)
+System.out.println("your number is even ");
+else
+System.out.println("your number is Odd  ");
+
+
+
+
+}
+
+
+}
